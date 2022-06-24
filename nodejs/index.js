ed1319bb9fe5c9b4ebebdbe775a9258205eb6fd8
shibichris <99465215+shibichris@users.noreply.github.com>
2022-06-24 09:33:08 +0530

    Express Js

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,11 +1,62 @@
-//Local <odule
+//Thirdparty module
 
-function myfunc()
+const express = require('express');
+
+const app = express();
+
+const morgan = require('morgan');
+
+const mongoose = require('mongoose');
+
+const uuid = require('uuid');
+
+
+//Middle ware
+
+app.use(morgan('dev'));
+
+const myPersons=[{
+    id:uuid.v4(),
+    name:"Shibin",
+    age:23
+},
 {
-    console.log("Function Working");
+    id:uuid.v4(),
+    name:"Peter",
+    age:24
+},
+{
+    id:uuid.v4(),
+    name:"Kevin",
+    age:25
 }
+]
 
-//myfunc();
 
-module.exports = myfunc;
 
+//get
+
+app.get('/',(req,res)=>{
+    res.status(400).json(myPersons);
+})
+
+//about
+
+app.get('/:id',(req,res)=>{
+    res.json(req.params.id);
+})
+
+//Local host
+
+app.listen(2000,()=>{
+    console.log('Server Started on 2000');
+});
+
+//DB server creation
+
+mongoose.connect('mongodb://localhost:27017/myapp',(err)=>{
+    if(err){
+        console.log('db not connected')
+    }
+    console.log('DB connected succesfully');
+})
