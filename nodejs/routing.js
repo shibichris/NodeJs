c2369a9b48fb410abf34ed513b1cc835d49f7544
shibichris <99465215+shibichris@users.noreply.github.com>
2022-06-27 16:54:31 +0530

    Added

---
+++ nodejs/routing.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.get('/', (req, res) => {
+    res.send('GET request to the homepage')
+  })
+  
+app.post('/', (req, res) => {
+    res.send('POST request to the homepage')
+  })
