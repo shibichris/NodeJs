5c4b847d2618582938e1729e1d31853b18640469
shibichris <99465215+shibichris@users.noreply.github.com>
2022-06-27 09:18:18 +0530

    Get, Post, Delete

--- nodejs/personsRoute.js
+++ nodejs/personsRoute.js
@@ -16,18 +16,30 @@ router.post('/',async(req,res)=>{
 
 //Get Persons
 
-router.get('/:id',async(req,res)=>{
+router.get('/',async(req,res)=>{
     const getPersons = await Persons.find();
     res.status(200).json(getPersons);
 })
 
 //Get By Id
 
-router.get('/',async(req,res)=>{
+router.get('/:id',async(req,res)=>{
     const getById = await Persons.findById(req.params.id);
     res.status(200).json(getById);
 })
 
+//Update 
 
+router.put('/:id',async(req,res)=>{
+    const updatePersons =await Persons.updateOne({_id:req.params.id},{$set:{Name:req.body.Name,Age:req.body.Age}})
+    res.status(200).json(updatePersons);
+})
+
+//Delete
+
+router.delete('/:id',async(req,res)=>{
+    const delPersons = await Persons.remove({_id:req.params.id});
+    res.status(200).json(delPersons);
+})
 
 module.exports = router;
